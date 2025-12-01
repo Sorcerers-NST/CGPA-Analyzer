51037bdaf993a9bba2d293b878dd10bcfb99470d
Bitshifter-9 <bandarampranav@gmail.com>
2025-12-01 09:38:38 +0530

    fixed errors

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,7 +3,17 @@ export default {
     "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        navy: {
+          600: '#1e3a5f',
+          700: '#15283e',
+          800: '#0d1929',
+        }
+      }
+    },
+  },
   plugins: [],
-}}
+}
